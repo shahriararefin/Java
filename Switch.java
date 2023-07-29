61d900a8c38f7bcfc8dab13910317438ebc1b768
shahriararefin <shahriararefin7@gmail.com>
2023-07-29 11:58:17 +0600

    commit

---
+++ Switch.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Switch {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        // if (button == 1) {
+        //     System.out.println("Hello");
+        // } 
+        
+        // else if(button == 2){
+        //     System.out.println("AsslamuAlaikum");
+        // } 
+        
+        // else if (button ==3) {
+        //     System.out.println("Bonjour");
+        // }
+        
+        // else{
+        //     System.out.println("Invalid Button");
+        // }
+
+        switch(button){
+            case 1: System.out.println("Hello");
+            break;
+
+            case 2: System.out.println("Assalamu Alaikum");
+            break;
+
+            case 3: System.out.println("Bonjour");
+            break;
+
+            default: System.out.println("Invalid Button");
+        }
+    }
+}
