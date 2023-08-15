9ece517fbe175ae05a1807c3df05ce5d71c21c2e
shahriararefin <shahriararefin7@gmail.com>
2023-08-15 14:05:19 +0600

    commit

---
+++ Loops.java
@@ -0,0 +1,18 @@
+public class Loops {
+    public static void main(String[] args) {
+        
+        // for(int n= 0; n<3;n++){
+        //     System.out.println("Hello");
+
+        // }
+
+
+        for (int i = 1; i <= 4; i++) {
+
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    sum = a+b;
+
+}
+
+
+public static void main(String[] args) {
+    
+}
